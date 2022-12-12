98e77b73c269fc403a435e37f820022a8c9608ba
dionardomarques <dionardogianluca@gmail.com>
2022-12-11 23:36:45 -0300

    PostsPage done

--- frontend/src/Pages/layout.jsx
+++ frontend/src/Pages/layout.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Layout from "../components/Layout";
+import Jobs from "../components/Layout";
 
 function LayoutScreen() {
-	return <Layout></Layout>;
+	return <Jobs></Jobs>;
 }
 
 export default LayoutScreen;
