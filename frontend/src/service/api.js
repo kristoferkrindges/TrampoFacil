7b8b27392b116b2cbf1277744f3c3d38e5994128
Kristofer R.K <55563253+kristoferkrindges@users.noreply.github.com>
2022-12-14 07:24:56 -0300

    Update api.js

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const Api = axios.create({ baseURL: "http://localhost:5000" });
+const Api = axios.create({ baseURL: "https://backend-blue-xi.vercel.app/" });
 
 export default Api;
